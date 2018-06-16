08aafc1ba885f1c8df298a332d82d6d1585fac98
Katharine Cordes <gillis.katharine@gmail.com>
2018-06-16 02:07:47 -0700

    Webpack setup added.

--- Dockerfile
+++ Dockerfile
@@ -2,4 +2,6 @@ FROM nginx:1.15.0-alpine
 
 COPY ./html /usr/share/nginx/html
 
+RUN mv /usr/share/nginx/html/index_prod.html /usr/share/nginx/html/index.html
+
 EXPOSE 80
---
+++ src/App.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './app.scss';
+
+ReactDOM.render(<p>Coming soon!</p>, document.getElementById('root'));
