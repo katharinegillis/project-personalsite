e015ac87c828ab3ee7ad223fa9efd6550421bcbe
Katharine Cordes <gillis.katharine@gmail.com>
2018-11-25 15:52:38 -0700

    Fix compose project name in scripts

--- bitbucket-pipelines-update-dev.sh
+++ bitbucket-pipelines-update-dev.sh
@@ -2,7 +2,7 @@
 
 cd /docker/personalsite
 
-set COMPOSE_PROJECT_NAME=personalsite-dev
+export COMPOSE_PROJECT_NAME=personalsite-dev
 
 docker-compose -f docker-compose.yml -f docker-compose.dev.yml pull
 docker-compose -f docker-compose.yml -f docker-compose.dev.yml up -d --remove-orphans
--- bitbucket-pipelines-update-prod.sh
+++ bitbucket-pipelines-update-prod.sh
@@ -2,7 +2,7 @@
 
 cd /docker/personalsite
 
-set COMPOSE_PROJECT_NAME=personalsite
+export COMPOSE_PROJECT_NAME=personalsite
 
 docker-compose -f docker-compose.yml -f docker-compose.prod.yml pull
 docker-compose -f docker-compose.yml -f docker-compose.prod.yml up -d --remove-orphans
