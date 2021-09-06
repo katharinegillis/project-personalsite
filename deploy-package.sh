0f61df5aa214054fab51c2e203a9c0f0097f52ee
Katharine Cordes <gillis.katharine@gmail.com>
2021-09-05 22:41:31 -0600

    fix: adjust deploy script for new deployment to staging

--- deploy-package.sh
+++ deploy-package.sh
@@ -2,8 +2,5 @@
 
 tar -czf deploy.tar.gz \
   deploy.sh \
-  switch.sh \
-  .docker \
   docker-compose.prod.yml \
-  docker-compose.yml \
-  docker-compose.prod-ssl.yml
+  docker-compose.yml
--- deploy.sh
+++ deploy.sh
@@ -1,39 +1,21 @@
 #!/bin/bash
 
-# Recreate the .env file
-rm .env
-touch .env
-{
-  echo SITE_URL="${SITE_URL}"
-  echo SMTP_FROM_EMAIL="${SMTP_FROM_EMAIL}"
-  echo SMTP_HOST="${SMTP_HOST}"
-  echo SMTP_USER="${SMTP_USER}"
-  echo SMTP_PASSWORD="${SMTP_PASSWORD}"
-  echo CONTACT_EMAIL="${CONTACT_EMAIL}"
-} >> .env
-
 # Determine which colour is staging
-if grep -q 'proxy_pass http://green-backend:8080;' .docker/ingress/conf.d/default-staging.conf
+if grep -q 'set \$backend green:8080;' ../.docker/ingress/conf.d/default.conf
 then
     STAGING="green"
 else
     STAGING="blue"
 fi
 
+# Recreate the .env file
+rm .env
+touch .env
+{
+  echo TRAEFIK_NAME="personalsite"
+  echo SITE_URL="${SITE_URL}"
+} >> .env
+
 # Update staging
-if [ $STAGING = "green" ];
-then
-  docker-compose -f docker-compose.yml -f docker-compose.prod.yml -f docker-compose.prod-ssl.yml pull \
-    green-backend \
-    green-phpfpm
-  docker-compose -f docker-compose.yml -f docker-compose.prod.yml -f docker-compose.prod-ssl.yml up -d \
-    green-backend \
-    green-phpfpm
-else
-  docker-compose -f docker-compose.yml -f docker-compose.prod.yml -f docker-compose.prod-ssl.yml pull \
-    blue-backend \
-    blue-phpfpm
-  docker-compose -f docker-compose.yml -f docker-compose.prod.yml -f docker-compose.prod-ssl.yml up -d \
-    blue-backend \
-    blue-phpfpm
-fi
+docker-compose -f docker-compose.yml -f docker-compose.prod.yml -f "../docker-compose.$STAGING.yml" pull
+docker-compose -f docker-compose.yml -f docker-compose.prod.yml -f "../docker-compose.$STAGING.yml" up -d
